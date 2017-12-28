04c932db7adc730b1aa0283171352a7021ed6579
David Galloway <davidcgalloway@gmail.com>
2017-12-28 09:35:34 -0800

    Kind of monkey programmed my way towards getting the correct search query back from google image search - for some reason it was returning an /webhp path instead of /search

--- Snipping OCR/ImageSearchHelper/ImageSearchHelper.cs
+++ Snipping OCR/ImageSearchHelper/ImageSearchHelper.cs
@@ -25,21 +25,19 @@ namespace Snipping_OCR
             try
             {
 
-                var searchUrl = "http://www.google.hr/searchbyimage/upload";
-
-                //multipart = { 'encoded_image': (imagefile, open(imagefile, 'rb')), 'image_content': ''}
-                //response = requests.post(searchUrl, files = multipart, allow_redirects = False)
-                //fetchUrl = response.headers['Location']
-                //print fetchUrl
-
-                RestRequest request = new RestRequest("encoded_image", Method.POST);
-                request.AddParameter("filename=", File.ReadAllBytes(@"C:\Temp\Capture.png"), ParameterType.RequestBody);
-                request.AddHeader("Content-Type", "image/png");
-                //request.AddHeader("image_content", "");
-
+                var searchUrl = "http://www.google.ca/searchbyimage/upload";
+                RestRequest request = new RestRequest(Method.POST);
+                request.AddFile("encoded_image", File.ReadAllBytes(filePath), "");
+                request.AlwaysMultipartFormData = true;
                 var client = new RestClient(new Uri(searchUrl));
-
                 var response = client.Execute(request);
+
+
+                string url = response.ResponseUri.Scheme + "://" + response.ResponseUri.Host + "/search" + response.ResponseUri.Query;
+
+
+                System.Diagnostics.Process.Start(url);
+
 
                 var text = "dummy";
                 return new OcrResult()
